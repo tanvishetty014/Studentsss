c897813e950d435c8240bcfb2da2b8403fbf30fd
tanvishetty014 <tanvishetty407@gmail.com>
2025-04-22 14:31:44 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student{
+  public static void main(String[] args) {
+       String name ="tanvi";
+       int srn = 87;
+        String dept = "BCA";
+        int age = 20;
+        long no = 9741311572;
+
+        System.out.println("Student Deails");
+        System.out.println("---------------");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Student registration number: "+srn);
+        System.out.println("Department: "+dept);
+        System.out.println("phone number: "+no);
+
+  }
+}
